85a035d21c78ebac02c4d3a2be998a7a46bc13ee
td041 <trinhtrantrungduc@gmail.com>
2024-11-26 14:55:34 +0700

    Set up ReactRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { store } from "./redux/store.js";
-
-createRoot(document.getElementById("root")).render(
+import { Provider } from "react-redux";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App></App>
     </Provider>
   </React.StrictMode>
 );
